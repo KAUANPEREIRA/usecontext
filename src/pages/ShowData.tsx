ff2cb203ff90c0021455f38893dee6910ac7552c
KAUANSANTOS <66709503+KAUANPEREIRA@users.noreply.github.com>
2023-03-16 18:51:45 -0300

    useReducer

--- src/pages/ShowData.tsx
+++ src/pages/ShowData.tsx
@@ -1,9 +1,66 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { usePeopleList } from "../reducers/peopleList";
 const ShowData = () => {
+  const [nameInput, setNameInput] = useState("");
+  const [list, dispatch] = usePeopleList();
+  const handleAddButton = () => {
+    if (nameInput) {
+      dispatch({
+        type: "ADD",
+        payload: {
+          name: nameInput,
+        },
+      });
+    }
+  };
+
+  console.log({ nameInput });
+
+  const HandleDeletePerson = (id: string) => {
+    if (id) {
+      dispatch({
+        type: "DEL",
+        payload: {
+          id: id,
+        },
+      });
+    }
+  };
+
+  const handleOrder = () => {
+    dispatch({
+      type: "ORDER",
+    });
+  };
+
   return (
     <div>
       Componente show data
+      <h4>adicionar um usuario</h4>
+      <div>
+        <input
+          type="text"
+          value={nameInput}
+          onChange={(e) => setNameInput(e.target.value)}
+        />
+        <button onClick={handleAddButton}>Adicionar</button>
+        <button onClick={handleOrder}>Ordenar</button>
+      </div>
       <hr></hr>
+      <h1>Lista com reducer </h1>
+      <ul>
+        {list.map((item, index) => {
+          return (
+            <li key={index}>
+              {item?.name}{" "}
+              <button onClick={() => HandleDeletePerson(item?.id)}>
+                Deletar
+              </button>
+            </li>
+          );
+        })}
+      </ul>
       <Link to="/">Voltar para cadastro</Link>
     </div>
   );
--- src/reducers/peopleList.ts
+++ src/reducers/peopleList.ts
@@ -1,3 +1,5 @@
+import { useReducer } from "react";
+import { v4 as uuiv4 } from "uuid";
 type Person = {
   id: string;
   name: string;
@@ -16,13 +18,33 @@ const initialState: Person[] = [];
 const reducer = (state: Person[], action: ActionType) => {
   switch (action.type) {
     case "ADD":
+      if (action.payload?.name) {
+        const newState = [...state];
+        newState.push({
+          id: uuiv4(),
+          name: action?.payload?.name,
+        });
+
+        return newState;
+      }
+
       break;
     case "DEL":
+      if (action.payload?.id) {
+        let newState = [...state];
+
+        newState = newState?.filter((item) => item?.id !== action.payload?.id);
+        return newState;
+      }
       break;
     case "ORDER":
-      break;
+      let newState = [...state];
+      newState = newState.sort((a, b) => (a.name > b.name ? 1 : -1));
+      return newState;
   }
   return state;
 };
 //usePeopleList hook criado para utilizar o reducer
-export const usePeopleList = () => {};
+export const usePeopleList = () => {
+  return useReducer(reducer, initialState);
+};
